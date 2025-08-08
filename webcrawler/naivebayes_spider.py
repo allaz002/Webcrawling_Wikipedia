002032aff9fc808bc90078eca5d6fff7a7ce2805
Alexander Lazarev <xxragepowerxxxxragepowerxx@gmail.com>
2025-08-09 01:23:30 +0200

    9t Commit

--- webcrawler/naivebayes_spider.py
+++ webcrawler/naivebayes_spider.py
@@ -1,37 +1,10 @@
 from webcrawler.base_spider import BaseTopicalSpider
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.preprocessing import MaxAbsScaler
 import pickle
 import os
 import json
 import numpy as np
-
-
-class TfNormVectorizer:
-    """TF-Norm Vektorisierer: f_t,d / max_t' f_t',d"""
-
-    def __init__(self, max_features=1000, ngram_range=(1, 2), min_df=1, max_df=0.95):
-        self.count_vectorizer = CountVectorizer(
-            max_features=max_features,
-            ngram_range=ngram_range,
-            min_df=min_df,
-            max_df=max_df
-        )
-        self.scaler = MaxAbsScaler()
-
-    def fit(self, documents):
-        count_matrix = self.count_vectorizer.fit_transform(documents)
-        self.scaler.fit(count_matrix)
-        return self
-
-    def transform(self, documents):
-        count_matrix = self.count_vectorizer.transform(documents)
-        return self.scaler.transform(count_matrix)
-
-    def fit_transform(self, documents):
-        count_matrix = self.count_vectorizer.fit_transform(documents)
-        return self.scaler.fit_transform(count_matrix)
 
 
 class NaiveBayesSpider(BaseTopicalSpider):
@@ -46,7 +19,7 @@ class NaiveBayesSpider(BaseTopicalSpider):
         self.vectorizer_path = self.config['NAIVEBAYES']['VECTORIZER_PATH']
         self.training_data_path = self.config['NAIVEBAYES']['TRAINING_DATA_PATH']
 
-        self.mode = self.config['NAIVEBAYES'].get('MODE', 'tfidf').lower()
+        self.mode = self.config['NAIVEBAYES'].get('MODE', 'tf').lower()
 
         # Gewichtungen aus WEIGHTS Sektion
         self.title_weight = float(self.config['WEIGHTS']['TITLE_WEIGHT'])
@@ -89,27 +62,30 @@ class NaiveBayesSpider(BaseTopicalSpider):
         if not texts:
             raise ValueError("Keine gültigen Trainingsdaten vorhanden!")
 
-        # Initialisiere Vectorizer basierend auf Modus mit separaten Settings
-        if self.mode == 'tf_norm':
-            vectorizer_config = self.config['VECTORIZER_NB_TF_NORM']
-            self.vectorizer = TfNormVectorizer(
+        # Initialisiere Vectorizer basierend auf Modus
+        if self.mode == 'tf':
+            # Reine Termhäufigkeiten ohne Normalisierung
+            vectorizer_config = self.config['VECTORIZER_TF_NORM']
+            self.vectorizer = CountVectorizer(
                 max_features=int(vectorizer_config['MAX_FEATURES']),
                 ngram_range=(int(vectorizer_config['NGRAM_MIN']),
                              int(vectorizer_config['NGRAM_MAX'])),
                 min_df=int(vectorizer_config['MIN_DF']),
                 max_df=float(vectorizer_config['MAX_DF'])
             )
-            print("Verwende TF-Norm Vektorisierung")
+            print("Verwende reine Termhäufigkeiten (TF) ohne Normalisierung")
         else:  # tfidf
-            vectorizer_config = self.config['VECTORIZER_NB_TFIDF']
+            # TF-IDF mit L2-Normalisierung (Standard)
+            vectorizer_config = self.config['VECTORIZER_TFIDF']
             self.vectorizer = TfidfVectorizer(
                 max_features=int(vectorizer_config['MAX_FEATURES']),
                 ngram_range=(int(vectorizer_config['NGRAM_MIN']),
                              int(vectorizer_config['NGRAM_MAX'])),
                 min_df=int(vectorizer_config['MIN_DF']),
-                max_df=float(vectorizer_config['MAX_DF'])
+                max_df=float(vectorizer_config['MAX_DF']),
+                norm='l2'  # Dokumentweise L2-Normalisierung
             )
-            print("Verwende TF-IDF Vektorisierung")
+            print("Verwende TF-IDF mit dokumentweiser L2-Normalisierung")
 
         # Vektorisiere und trainiere
         X = self.vectorizer.fit_transform(texts)
